f685979543d772cd4cbe64b0eb4e6d7dc172c879
Gerard Doncel <72249702+umigam3@users.noreply.github.com>
2025-01-22 18:03:37 +0100

    updated hero

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -6,55 +6,27 @@ import Carousel from "@components/hero/Carousel";
 
 export default function Hero() {
   return (
-    <section id="hero" className="w-full pt-8 mb-16">
+    <section id="hero" className="w-full pt-8 mb-72 mt-24">
       <div className="relative flex flex-col mb-16">
-        <img
+        {/* <img
           style={{
             maskImage: "linear-gradient(to bottom, black 50%, transparent)",
           }}
           className="absolute w-[500px] right-14 mask-gradient-to-bottom"
           src="/gerard_alt.png"
-        ></img>
-        <div className="w-full lg:w-3/4 flex flex-col justify-center lg:mt-0 md:z-10">
-          <h1 className="md:leading-tight md:text-6xl text-5xl font-extrabold bg-gradient-to-r text-c mb-4 text-primary">
-            <span className="inline lg:block">Hello! I'm </span>
-            <span className="inline lg:block">Gerard Doncel a </span>
+        ></img> */}
+        <div className="w-full flex flex-col justify-center items-end lg:mt-0 md:z-10">
+          <h1 className="md:leading-tight md:text-6xl text-5xl font-extrabold bg-gradient-to-r text-right mb-4 text-primary">
+            <span className="inline lg:block text-secondary">
+              Full-Stack Developer
+            </span>
+            <span className="inline lg:block">PHP | Laravel | Next.js</span>
             <span className="text-secondary font-extrabold inline lg:block">
               Computer Engineer{" "}
-            </span>
-            <span className="inline lg:block">
-              with more than{" "}
-              <span className="text-secondary font-extrabold">2 years </span>
-            </span>
-            <span className="inline lg:block">
-              in{" "}
-              <span className="text-secondary font-extrabold">
-                web development{" "}
-              </span>
-            </span>
-            <span className="inline lg:block">
-              {" "}
-              and solid skills in{" "}
-              <span className="text-secondary font-extrabold">PHP</span>
-            </span>
+            </span>{" "}
           </h1>
-          <div className="flex flex-wrap sm:flex-nowrap gap-x-1">
-            <Button
-              href="https://www.linkedin.com/in/gerard-doncel-gutierrez/"
-              label="LinkedIn"
-            >
-              <LinkedInIcon />
-            </Button>
-            <Button href="https://github.com/umigam3" label="GitHub">
-              <GitHubIcon size={18} />
-            </Button>
-            <Button href="#" label="Resume">
-              <ResumeIcon size={18} />
-            </Button>
-          </div>
         </div>
       </div>
-      <Carousel />
     </section>
   );
 }
