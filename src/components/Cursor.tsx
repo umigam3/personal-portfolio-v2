88df199df9660d3ba8aaccde661aeb917b56df43
Gerard Doncel <72249702+umigam3@users.noreply.github.com>
2025-01-26 21:32:58 +0100

    cursor ajustments

--- src/components/Cursor.tsx
+++ src/components/Cursor.tsx
@@ -6,7 +6,7 @@ import { useEffect } from "react";
 
 export default function Cursor() {
   const { x, y } = useMousePosition();
-  const size = 750;
+  const size = 1000;
 
   useEffect(() => {
     console.log(x, y);
@@ -14,12 +14,16 @@ export default function Cursor() {
   return (
     <div className="w-full h-full fixed overflow-hidden -z-10">
       <motion.div
-        className="absolute h-[750px] w-[750px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(124,58,237,.25),rgba(255,255,255,0))]"
+        className="absolute h-[1000px] w-[1000px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(124,58,237,.30),rgba(255,255,255,0))]"
         animate={{
           x: x - size / 2,
           y: y - size / 2,
         }}
-        transition={{ type: "tween", ease: "backOut", duration: 0.5 }}
+        transition={{
+          type: "tween",
+          ease: "backOut",
+          duration: 1.5,
+        }}
       ></motion.div>
     </div>
   );
