d7f0d3c0d28b576efc4d65fef0b387f32ffa7030
Gerard Doncel <72249702+umigam3@users.noreply.github.com>
2025-02-14 20:19:18 +0100

    wip project section

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -6,26 +6,43 @@ export default function Experience() {
     <section id="projects" className="mb-20 w-full">
       <Title label="Projects" />
       <div className="flex flex-col">
-        <div className="project w-full cursor-pointer hover:bg-secondary hover:text-black border-t-2 border-slate-800">
-          <div className="flex items-center justify-between max-w-[1560px] mx-auto">
-            <span className="pt-6 leading-[256px] title text-[256px] font-delirium z-10 ">
-              POKEDEX
-            </span>
-            <span className="description">
-              A pokemon application using react and consusuming the pokeapi.co
-              API
-            </span>
+        <div className="relative">
+          <div className="w-full cursor-pointer relative">
+            <div className="flex items-center justify-between max-w-[1560px] mx-auto">
+              <span className="pt-6 leading-[256px] title text-[256px] font-delirium z-10 ">
+                POKEDEX
+              </span>
+            </div>
+          </div>
+          <div className="project w-full cursor-pointer bg-secondary text-black absolute top-0 z-10">
+            <div className="flex items-center justify-between max-w-[1560px] mx-auto">
+              <span className="pt-6 leading-[256px] title text-[256px] font-delirium z-10 ">
+                POKEDEX
+              </span>
+              <span className="description">
+                A pokemon application using react and consusuming the pokeapi.co
+                API
+              </span>
+            </div>
           </div>
         </div>
-        <div className="w-full cursor-pointer hover:bg-secondary hover:text-black border-b-2 border-t-2 border-slate-800">
-          <div className="flex items-center justify-between max-w-[1560px] mx-auto">
-            <span className="pt-6 leading-[256px] title text-[256px] text-7xl font-delirium">
-              UNIX SCRIPTS
-            </span>
-            <span className="description">
-              A collection of scripts that I have written to automate tasks on
-              my
-            </span>
+        <div className="relative">
+          <div className="w-full cursor-pointer relative">
+            <div className="flex items-center justify-between max-w-[1560px] mx-auto">
+              <span className="pt-6 leading-[256px] title text-[256px] font-delirium z-10 ">
+                UNIX SCRIPTS
+              </span>
+            </div>
+          </div>
+          <div className="project w-full cursor-pointer bg-secondary text-black absolute top-0 z-10">
+            <div className="flex items-center justify-between max-w-[1560px] mx-auto">
+              <span className="pt-6 leading-[256px] title text-[256px] font-delirium z-10 ">
+                UNIX SCRIPTS
+              </span>
+              <span className="description">
+                A collection of scripts that I have written to automate tasks.
+              </span>
+            </div>
           </div>
         </div>
       </div>
