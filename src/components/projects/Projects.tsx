1225d6a42ec49b58b80d42a01a40810a657226ed
Gerard Doncel <72249702+umigam3@users.noreply.github.com>
2025-02-21 19:44:39 +0100

    links to projects

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -9,50 +9,58 @@ export default function Experience() {
     <section id="projects" className="mb-20 w-full">
       <Title label="Projects" />
       <div className="flex flex-col">
-        <div className="relative cursor-pointer">
-          <div className="w-full relative border-t border-slate-400">
-            <div className="flex items-center justify-between max-w-7xl mx-auto">
-              <span className="pt-3 leading-[256px] title text-[256px] font-delirium z-10 w-2/3 max-w-7xl">
-                POKEDEX
-              </span>
-            </div>
-          </div>
-          <div className="project w-full bg-secondary text-black absolute top-0 z-10 border-t border-secondary">
-            <div className="flex justify-between max-w-7xl mx-auto">
-              <span className="pt-3 leading-[256px] title text-[256px] font-delirium z-10 w-2/3">
-                POKEDEX
-              </span>
-              <div className="flex flex-col justify-start w-1/3 items-start">
-                <span className="description text-2xl text-right pt-10">
-                  Simple Pokedex application built using React. It utilizes the
-                  PokeAPI to fetch and display Pokémon information.
+        <a href="https://github.com/umigam3/pokedex-api" target="_blank">
+          <div className="relative cursor-pointer">
+            <div className="w-full relative border-t border-slate-400">
+              <div className="flex items-center justify-between max-w-7xl mx-auto">
+                <span className="pt-3 leading-[256px] title text-[256px] font-delirium z-10 w-2/3 max-w-7xl">
+                  POKEDEX
                 </span>
               </div>
             </div>
-          </div>
-        </div>
-        <div className="relative cursor-pointer">
-          <div className="w-full relative border-t border-slate-400 border-b">
-            <div className="flex items-center justify-between max-w-7xl mx-auto">
-              <span className="pt-3 leading-[256px] title text-[256px] font-delirium z-10">
-                UNIX SCRIPTS
-              </span>
-            </div>
-          </div>
-          <div className="project w-full bg-secondary text-black absolute top-0 z-10 border-t border-secondary">
-            <div className="flex justify-between max-w-7xl mx-auto">
-              <span className="pt-3 leading-[256px] title text-[256px] font-delirium z-10 w-2/3">
-                UNIX SCRIPTS
-              </span>
-              <div className="flex flex-col justify-start w-1/3 items-start">
-                <span className="description text-2xl text-right pt-10">
-                  Collection of Bash scripts designed to enhance system
-                  administration and user management tasks on Unix-like systems.
+            <div className="project w-full bg-secondary text-black absolute top-0 z-10 border-t border-secondary">
+              <div className="flex justify-between max-w-7xl mx-auto">
+                <span className="pt-3 leading-[256px] title text-[256px] font-delirium z-10 w-2/3">
+                  POKEDEX
                 </span>
+                <div className="flex flex-col justify-start w-1/3 items-start">
+                  <span className="description text-2xl text-right pt-10">
+                    Simple Pokedex application built using React. It utilizes
+                    the PokeAPI to fetch and display Pokémon information.
+                  </span>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </a>
+        <a
+          href="https://github.com/umigam3/unix-system-scripts"
+          target="_blank"
+        >
+          <div className="relative cursor-pointer">
+            <div className="w-full relative border-t border-slate-400 border-b">
+              <div className="flex items-center justify-between max-w-7xl mx-auto">
+                <span className="pt-3 leading-[256px] title text-[256px] font-delirium z-10">
+                  UNIX SCRIPTS
+                </span>
+              </div>
+            </div>
+            <div className="project w-full bg-secondary text-black absolute top-0 z-10 border-t border-secondary">
+              <div className="flex justify-between max-w-7xl mx-auto">
+                <span className="pt-3 leading-[256px] title text-[256px] font-delirium z-10 w-2/3">
+                  UNIX SCRIPTS
+                </span>
+                <div className="flex flex-col justify-start w-1/3 items-start">
+                  <span className="description text-2xl text-right pt-10">
+                    Collection of Bash scripts designed to enhance system
+                    administration and user management tasks on Unix-like
+                    systems.
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </a>
       </div>
     </section>
   );
