2182edfa70a40b0c42be6da1b996180d6c42e896
Gerard Doncel <72249702+umigam3@users.noreply.github.com>
2025-02-21 19:39:59 +0100

    new icons

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,4 +1,6 @@
+import Bash from "@components/icons/Bash";
 import JavaScript from "@components/icons/JavaScript";
+import Linux from "@components/icons/Linux";
 import React from "@components/icons/React";
 import Title from "@components/ui/Title";
 
@@ -7,7 +9,7 @@ export default function Experience() {
     <section id="projects" className="mb-20 w-full">
       <Title label="Projects" />
       <div className="flex flex-col">
-        <div className="relative">
+        <div className="relative cursor-pointer">
           <div className="w-full relative border-t border-slate-400">
             <div className="flex items-center justify-between max-w-7xl mx-auto">
               <span className="pt-3 leading-[256px] title text-[256px] font-delirium z-10 w-2/3 max-w-7xl">
@@ -16,24 +18,20 @@ export default function Experience() {
             </div>
           </div>
           <div className="project w-full bg-secondary text-black absolute top-0 z-10 border-t border-secondary">
-            <div className="flex items-center justify-between max-w-7xl mx-auto">
+            <div className="flex justify-between max-w-7xl mx-auto">
               <span className="pt-3 leading-[256px] title text-[256px] font-delirium z-10 w-2/3">
                 POKEDEX
               </span>
-              <div className="flex flex-col justify-between w-1/3 h-full items-end">
-                <span className="description text-2xl text-right">
+              <div className="flex flex-col justify-start w-1/3 items-start">
+                <span className="description text-2xl text-right pt-10">
                   Simple Pokedex application built using React. It utilizes the
                   PokeAPI to fetch and display Pokémon information.
                 </span>
-                <div className="flex gap-x-4 mt-4 rounded-3xl bg-black bg-opacity-50 px-6 py-3 w-fit">
-                  <React size={42} className="z-10" />
-                  <JavaScript size={42} />
-                </div>
               </div>
             </div>
           </div>
         </div>
-        <div className="relative">
+        <div className="relative cursor-pointer">
           <div className="w-full relative border-t border-slate-400 border-b">
             <div className="flex items-center justify-between max-w-7xl mx-auto">
               <span className="pt-3 leading-[256px] title text-[256px] font-delirium z-10">
@@ -42,19 +40,15 @@ export default function Experience() {
             </div>
           </div>
           <div className="project w-full bg-secondary text-black absolute top-0 z-10 border-t border-secondary">
-            <div className="flex items-center justify-between max-w-7xl mx-auto">
+            <div className="flex justify-between max-w-7xl mx-auto">
               <span className="pt-3 leading-[256px] title text-[256px] font-delirium z-10 w-2/3">
                 UNIX SCRIPTS
               </span>
-              <div className="flex flex-col justify-between w-1/3 h-full items-end">
-                <span className="description text-2xl text-right">
-                  Simple Pokedex application built using React. It utilizes the
-                  PokeAPI to fetch and display Pokémon information.
+              <div className="flex flex-col justify-start w-1/3 items-start">
+                <span className="description text-2xl text-right pt-10">
+                  Collection of Bash scripts designed to enhance system
+                  administration and user management tasks on Unix-like systems.
                 </span>
-                <div className="flex gap-x-4 mt-4 rounded-3xl bg-black bg-opacity-50 px-6 py-3 w-fit">
-                  <React size={42} className="z-10" />
-                  <JavaScript size={42} />
-                </div>
               </div>
             </div>
           </div>
