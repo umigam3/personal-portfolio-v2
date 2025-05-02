798b5235ccdfc73beb3cc989a7927f2aabff3db4
Gerard Doncel <72249702+umigam3@users.noreply.github.com>
2025-05-02 19:03:33 +0200

    project section wip

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -3,19 +3,27 @@ import Title from "@components/ui/Title";
 export default function Projects() {
   return (
     <section id="projects" className="mb-20 w-full">
-      <Title label="Projects" />
+      <h2 className="px-8 md:px-[5.5rem] max-w-[1520px] mx-auto uppercase mb-8 opacity-50 font-medium tracking-widest text-xl">
+        Projects
+      </h2>
       <div className="flex flex-col">
         <a href="https://github.com/umigam3/pokedex-api" target="_blank">
           <div className="relative cursor-pointer">
             <div className="w-full relative border-t border-slate-400">
-              <div className="flex items-center justify-between max-w-7xl mx-auto">
-                <span className="pt-3 leading-[256px] title text-[256px] font-delirium z-10 w-2/3 max-w-7xl">
+              <div className="flex items-center justify-between px-8 md:px-[5.5rem] max-w-[1520px] mx-auto">
+                <span className="pt-3 leading-[256px] title text-[256px] font-delirium z-10 w-2/3">
                   POKEDEX
                 </span>
+                <div className="flex flex-col justify-start w-1/3 items-start">
+                  <span className="description text-2xl text-right pt-10">
+                    Simple Pokedex application built using React. It utilizes
+                    the PokeAPI to fetch and display Pokémon information.
+                  </span>
+                </div>
               </div>
             </div>
             <div className="project w-full bg-gradient-to-r from-[#F47B2A] to-[#FFC76B] text-black absolute top-0 z-10 border-t border-secondary">
-              <div className="flex justify-between max-w-7xl mx-auto">
+              <div className="flex justify-between px-8 md:px-[5.5rem] max-w-[1520px] mx-auto">
                 <span className="pt-3 leading-[256px] title text-[256px] font-delirium z-10 w-2/3">
                   POKEDEX
                 </span>
@@ -35,14 +43,21 @@ export default function Projects() {
         >
           <div className="relative cursor-pointer">
             <div className="w-full relative border-t border-slate-400 border-b">
-              <div className="flex items-center justify-between max-w-7xl mx-auto">
+              <div className="flex items-center justify-between px-8 md:px-[5.5rem] max-w-[1520px] mx-auto">
                 <span className="pt-3 leading-[256px] title text-[256px] font-delirium z-10">
                   UNIX SCRIPTS
                 </span>
+                <div className="flex flex-col justify-start w-1/3 items-start">
+                  <span className="description text-2xl text-right pt-10">
+                    Collection of Bash scripts designed to enhance system
+                    administration and user management tasks on Unix-like
+                    systems.
+                  </span>
+                </div>
               </div>
             </div>
             <div className="project w-full bg-gradient-to-r from-[#F47B2A] to-[#FFC76B] text-black absolute top-0 z-10 border-t border-secondary">
-              <div className="flex justify-between max-w-7xl mx-auto">
+              <div className="flex justify-between px-8 md:px-[5.5rem] max-w-[1520px] mx-auto">
                 <span className="pt-3 leading-[256px] title text-[256px] font-delirium z-10 w-2/3">
                   UNIX SCRIPTS
                 </span>
