a4097e111cb59725c655eebd7d345ba52328fa65
Gerard Doncel <72249702+umigam3@users.noreply.github.com>
2025-05-06 22:30:17 +0200

    margins

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -2,7 +2,10 @@ import Title from "@components/ui/Title";
 
 export default function Contact() {
   return (
-    <section id="contact" className="mb-20 w-full">
+    <section
+      id="contact"
+      className="mb-14 md:mb-20 max-w-[1520px] mx-auto w-full px-5 md:px-[5.5rem]"
+    >
       <Title label="Contact" />
     </section>
   );
