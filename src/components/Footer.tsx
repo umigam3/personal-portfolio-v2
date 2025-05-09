3e9ee82990addfdb03c5b67e2cea2e7704a77a49
Gerard Doncel <72249702+umigam3@users.noreply.github.com>
2025-05-09 19:41:54 +0200

    footer font size

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div className="flex justify-center items-center bg-[#191919] px-5">
-      <span className="text-center text-[8px] md:text-sm text-gray-400 dark:text-gray-500 py-1">
+      <span className="text-center text-[10px] md:text-sm text-gray-400 dark:text-gray-500 py-1">
         Loosely designed in Figma and built with Next.js and Tailwind CSS - If
         you want you can follow me on{" "}
         <a className="text-white" href="https://github.com/umigam3">
