100163d46405d8a3361e1a3e4decc24ead06a5fb
Gerard Doncel <72249702+umigam3@users.noreply.github.com>
2025-05-17 00:57:24 +0200

    background gradient change

--- src/components/experience/ExperienceCard.tsx
+++ src/components/experience/ExperienceCard.tsx
@@ -24,7 +24,7 @@ export default function ExperienceCard({
                 : "lg:w-[47.5%] lg:text-right lg:border-l-violet-500/50 lg:border-l-[1px] lg:border-r-0 border-l-0 border-r-violet-500/50 border-r-[1px]"
             }`}
       >
-        <span className="px-4 bg-gradient-to-r from-[#F47B2A] to-[#FFC76B] inline-block text-transparent bg-clip-text text-xl font-bold mb-1">
+        <span className="px-4 bg-gradient-to-r from-[#FFC76B] to-[#F47B2A] inline-block text-transparent bg-clip-text text-xl font-bold mb-1">
           {position}
         </span>
         <span className="px-4 block mb-1 text-sm leading-none text-gray-400 dark:text-gray-500">
