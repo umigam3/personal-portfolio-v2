db30ba8d6b172e740606c4c855d656751a9d7694
Gerard Doncel <72249702+umigam3@users.noreply.github.com>
2025-05-17 00:02:23 +0200

    clean comments

--- src/components/hero/Time.tsx
+++ src/components/hero/Time.tsx
@@ -3,19 +3,17 @@ type Props = {
 };
 
 const Time = ({ time }: Props) => {
-  // Obtén el string de tiempo con AM/PM (sin espacios extra)
   const formattedTime = time
     .toLocaleTimeString(undefined, {
       hour: "2-digit",
       minute: "2-digit",
       hour12: true,
     })
-    .replace(/\./g, "") // eliminar puntos
-    .replace(/([AP])\s?M/i, "$1M"); // corregir espacio entre A M o P M
+    .replace(/\./g, "")
+    .replace(/([AP])\s?M/i, "$1M");
 
-  // Separar el "AM" o "PM" del resto
-  const timeWithoutAmPm = formattedTime.slice(0, -2); // todo menos últimos 2 caracteres
-  const amPm = formattedTime.slice(-2); // últimos 2 caracteres (AM o PM)
+  const timeWithoutAmPm = formattedTime.slice(0, -2);
+  const amPm = formattedTime.slice(-2);
 
   return (
     <span className="flex gap-2 items-start text-7xl tabular-nums font-poppins uppercase font-semibold">
