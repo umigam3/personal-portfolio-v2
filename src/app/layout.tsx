af4e8273cd1c569e279967a0e624e0256a862182
Gerard Doncel <72249702+umigam3@users.noreply.github.com>
2025-05-22 22:44:40 +0200

    new floating menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark overflow-x-hidden">
+    <html lang="en" className="dark">
       <body>
         <SmoothScrolling>
           <SocialsDesktop />
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -13,148 +13,132 @@ export default function Header() {
   const { scrollYProgress } = useScroll();
   const { lenis, scrollToTop, scrollToBottom, scrollToId } = useSmoothScroll();
 
-  useEffect(() => {
-    if (!lenis) return;
-
-    if (isOpen) {
-      lenis.stop();
-    } else {
-      lenis.start();
-    }
-  }, [isOpen, lenis]);
-
   return (
-    <header className="sticky top-0 z-10 h-20 backdrop-blur-xl">
-      <div className="flex h-full px-5 md:px-[5.5rem] justify-between items-center">
-        <a href="#">
-          <img
-            onClick={scrollToTop}
-            src="/signature.png"
-            alt="Gerard Doncel"
-            className="w-20"
-          />
-        </a>
-        <div className="hidden md:flex items-center gap-10">
-          <nav className="flex justify-end items-center gap-x-10">
-            <a className="cursor-pointer" onClick={() => scrollToId("aboutme")}>
-              About
-            </a>
-            <a
-              className="cursor-pointer"
-              onClick={() => scrollToId("experience")}
-            >
-              Experience
-            </a>
-            <a
-              className="cursor-pointer"
-              onClick={() => scrollToId("projects")}
-            >
-              Projects
-            </a>
-          </nav>
-          <a
-            onClick={() => scrollToBottom()}
-            className="bg-primary text-white px-6 py-2 rounded-xl cursor-pointer"
-          >
-            Get in touch
+    <>
+      <header className="sticky top-0 h-20 backdrop-blur-xl z-10">
+        <div className="flex h-full px-5 md:px-[5.5rem] justify-between items-center">
+          <a href="#">
+            <img
+              onClick={scrollToTop}
+              src="/signature.png"
+              alt="Gerard Doncel"
+              className="w-20"
+            />
           </a>
-        </div>
-
-        <a className="md:hidden z-50" onClick={() => setIsOpen(!isOpen)}>
-          {isOpen ? (
-            <Cross className="w-8 h-8" />
-          ) : (
-            <Hamburger className="w-8 h-8" />
-          )}
-        </a>
-
-        <AnimatePresence>
-          {isOpen && (
-            <>
-              <motion.div
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-                transition={{ duration: 0.3 }}
-                className="fixed inset-0 h-screen bg-black/70 z-30 md:hidden touch-none"
-                onClick={() => {
-                  setIsOpen(false);
-                }}
-              />
-              <motion.div
-                key="mobile-menu"
-                initial={{ opacity: 0, scale: 0.95 }}
-                animate={{ opacity: 1, scale: 1 }}
-                exit={{ opacity: 0, scale: 0.95 }}
-                transition={{ duration: 0.2, ease: "easeOut" }}
-                className="fixed top-0 right-0 w-3/4 max-w-xs h-dvh bg-[#191919] flex flex-col items-start justify-between px-8 gap-y-6 py-6 md:hidden shadow-lg z-40 border-l border-primary"
+          <div className="hidden md:flex items-center gap-10">
+            <nav className="flex justify-end items-center gap-x-10">
+              <a
+                className="cursor-pointer"
+                onClick={() => scrollToId("aboutme")}
               >
-                <div className="flex flex-col w-full">
-                  <img
-                    src="/signature.png"
-                    alt="Gerard Doncel"
-                    className="w-20 mb-10"
-                  />
-                  <div className="grid grid-rows-[4rem_1px_4rem_1px_4rem] w-full items-center">
-                    <a
-                      className="text-white text-lg"
-                      onClick={() => {
-                        setIsOpen(false);
-                        setTimeout(() => {
-                          scrollToId("aboutme");
-                        }, 1);
-                      }}
-                    >
-                      About me
-                    </a>
-                    <div className="h-px bg-white/50 w-full"></div>
-                    <a
-                      className="text-white text-lg"
-                      onClick={() => {
-                        setIsOpen(false);
-                        setTimeout(() => {
-                          scrollToId("experience");
-                        }, 1);
-                      }}
-                    >
-                      Experience
-                    </a>
-                    <div className="h-px bg-white/50 w-full"></div>
-                    <a
-                      className="text-white text-lg"
-                      onClick={() => {
-                        setIsOpen(false);
-                        setTimeout(() => {
-                          scrollToId("projects");
-                        }, 1);
-                      }}
-                    >
-                      Projects
-                    </a>
-                    <div className="h-px bg-white/50 w-full"></div>
-                  </div>
-                </div>
+                About
+              </a>
+              <a
+                className="cursor-pointer"
+                onClick={() => scrollToId("experience")}
+              >
+                Experience
+              </a>
+              <a
+                className="cursor-pointer"
+                onClick={() => scrollToId("projects")}
+              >
+                Projects
+              </a>
+            </nav>
+            <a
+              onClick={() => scrollToBottom()}
+              className="bg-primary text-white px-6 py-2 rounded-xl cursor-pointer"
+            >
+              Get in touch
+            </a>
+          </div>
+
+          <a className="md:hidden z-50" onClick={() => setIsOpen(!isOpen)}>
+            <Hamburger className="w-8 h-8" />
+          </a>
+
+          <motion.div
+            className="absolute bottom-0 left-0 h-0.5 w-full bg-gradient-to-r from-[#FFC76B] to-[#F47B2A] transform-gpu"
+            style={{ scaleX: scrollYProgress, originX: 0 }}
+          />
+        </div>
+      </header>
+      <AnimatePresence>
+        {isOpen && (
+          <>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.3 }}
+              className="fixed inset-0 h-screen md:hidden z-20"
+              onClick={() => {
+                setIsOpen(false);
+              }}
+            />
+            <motion.div
+              key="mobile-menu"
+              initial={{ opacity: 0, scale: 0.95 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.95 }}
+              transition={{ duration: 0.2, ease: "easeOut" }}
+              className="fixed top-24 right-5 w-2/4 max-w-xs flex flex-col items-start justify-between bg-black/10 p-4 md:hidden backdrop-blur-xl rounded-2xl bg-clip-padding backdrop-filter bg-opacity-10 border border-gray-500 z-40"
+            >
+              <div className="flex flex-col w-full gap-1.5">
                 <a
+                  className="text-white text-lg cursor-pointer"
                   onClick={() => {
                     setIsOpen(false);
                     setTimeout(() => {
-                      scrollToBottom();
+                      scrollToId("aboutme");
                     }, 1);
                   }}
-                  className="bg-primary text-white px-6 py-2 rounded-xl cursor-pointer flex items-center justify-center w-full"
                 >
-                  Get in touch
+                  About me
                 </a>
-              </motion.div>
-            </>
-          )}
-        </AnimatePresence>
-
-        <motion.div
-          className="absolute bottom-0 left-0 h-0.5 w-full bg-gradient-to-r from-[#FFC76B] to-[#F47B2A] transform-gpu"
-          style={{ scaleX: scrollYProgress, originX: 0 }}
-        />
-      </div>
-    </header>
+                <div className="h-px bg-gradient-to-r from-[#FFC76B] to-[#F47B2A] w-full"></div>
+                <a
+                  className="text-white text-lg cursor-pointer"
+                  onClick={() => {
+                    setIsOpen(false);
+                    setTimeout(() => {
+                      scrollToId("experience");
+                    }, 1);
+                  }}
+                >
+                  Experience
+                </a>
+                <div className="h-px bg-gradient-to-r from-[#FFC76B] to-[#F47B2A] w-full"></div>
+                <a
+                  className="text-white text-lg cursor-pointer"
+                  onClick={() => {
+                    setIsOpen(false);
+                    setTimeout(() => {
+                      scrollToId("projects");
+                    }, 1);
+                  }}
+                >
+                  Projects
+                </a>
+                <div className="h-px bg-gradient-to-r from-[#FFC76B] to-[#F47B2A] w-full"></div>
+                <a
+                  className="text-white text-lg cursor-pointer"
+                  onClick={() => {
+                    setIsOpen(false);
+                    setTimeout(() => {
+                      scrollToId("contact");
+                    }, 1);
+                  }}
+                >
+                  Contact
+                </a>
+                <div className="h-px bg-gradient-to-r from-[#FFC76B] to-[#F47B2A] w-full mb-1"></div>
+              </div>
+            </motion.div>
+          </>
+        )}
+      </AnimatePresence>
+    </>
   );
 }
